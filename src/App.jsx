436d92e16417960c3f42ab200b8254195ad1a5cb
Eduardo <el_alienx@hotmail.com>
2022-03-30 10:21:34 +0200

    clean up App

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-// NPM packages
-import { useState } from "react";
-
-// Project file
-import getNames from "./script/getNames";
-
 export default function App() {
-  // Local state
-  const [teachers, setTeachers] = useState([]);
-
-  // Components
-  const TeacherItems = teachers.map((item, index) => (
-    <li key={index}>{item}</li>
-  ));
-
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-      {teachers.length > 0 && <ol>{TeacherItems}</ol>}
-      <button onClick={() => setTeachers(getNames("Eduardo", 1))}>
-        Show teachers
-      </button>
+      <h1>Todo list</h1>
     </div>
   );
 }
